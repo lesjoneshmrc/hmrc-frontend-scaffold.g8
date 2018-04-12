f64692d6539322772bb0add135deef9521a7b1a1
Les Jones <les.jones@digital.hmrc.gov.uk>
2018-04-12 14:23:10 +0100

    Added extra questions to yes/no page scaffold

--- src/main/scaffolds/yesNoPage/migrations/$className__snake$.sh
+++ src/main/scaffolds/yesNoPage/migrations/$className__snake$.sh
@@ -13,10 +13,11 @@ echo "POST       /change$className$                       controllers.$className
 
 echo "Adding messages to conf.messages"
 echo "" >> ../conf/messages.en
-echo "$className;format="decap"$.title = $className;format="decap"$" >> ../conf/messages.en
-echo "$className;format="decap"$.heading = $className;format="decap"$" >> ../conf/messages.en
+echo "$className;format="decap"$.title = $title$" >> ../conf/messages.en
+echo "$className;format="decap"$.heading = $title$" >> ../conf/messages.en
 echo "$className;format="decap"$.checkYourAnswersLabel = $className;format="decap"$" >> ../conf/messages.en
 echo "$className;format="decap"$.error.required = Please give an answer for $className;format="decap"$" >> ../conf/messages.en
+echo "$className;format="decap"$.text = $text$" >> ../conf/messages.en
 
 echo "Adding helper line into UserAnswers"
 awk '/class/ {\
